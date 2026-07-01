39cb8a3ae564aa231b872ac922333c51f32d6338
Garrison Swinson <gb2520@berkeley.edu>
2026-07-01 13:52:01 -0700

    snapshot: local ATLAS build before syncing to GitHub

--- scripts/google-auth.js
+++ scripts/google-auth.js
@@ -44,6 +44,7 @@ async function main() {
     scope: [
       'https://www.googleapis.com/auth/calendar.readonly',
       'https://www.googleapis.com/auth/gmail.readonly',
+      'https://www.googleapis.com/auth/drive.readonly',
     ],
   });
 
